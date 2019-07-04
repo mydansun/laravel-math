36e7120401202df0c1f2c2d55dca96accd9f1fd3
Daniel Sun <mydansun@gmail.com>
2019-07-04 12:25:16 +0800

    correct class name

--- src/Math/Facades/MathFacade.php
+++ src/Math/Facades/MathFacade.php
@@ -3,7 +3,7 @@
 namespace Mydansun\Math\Facades;
 
 
-class Math extends \Illuminate\Support\Facades\Facade
+class MathFacade extends \Illuminate\Support\Facades\Facade
 {
     /**
      * Get the registered name of the component.
