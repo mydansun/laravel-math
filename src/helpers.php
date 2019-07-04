5927e002403874f1691975ecc96096d3cdc8be47
Daniel Sun <mydansun@gmail.com>
2019-07-04 14:56:49 +0800

    add number_display function

--- src/helpers.php
+++ src/helpers.php
@@ -16,3 +16,9 @@ function count_decimal($number)
 {
     return Math::getDecimalsLengthFromNumber($number);
 }
+
+function number_display($number, $decPoint = '.', $thousandSep = ',')
+{
+    $number = remove_end_zeros($number);
+    return number_format($number, count_decimal($number), $decPoint, $thousandSep);
+}
